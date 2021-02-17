4333febaa5584394630ae2f019021d47d8141799
smruti <smrutiranjanb44@gmail.com>
2021-02-17 09:01:46 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Planner'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text('Expense Planner'),
+      ),
+    );
+  }
+}
